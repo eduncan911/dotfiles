f771d2ec397aefaf2f80b4b34512009caa3176ca
Eric Duncan <eduncan911+github@gmail.com>
2016-07-21 17:23:44 -0400

    Go: more packages to upgrade

--- bin/upgrade-go.sh
+++ bin/upgrade-go.sh
@@ -7,6 +7,9 @@ go get -v -u github.com/golang/lint/golint
 go get -v -u github.com/redefiance/go-find-references
 go get -v -u github.com/jstemmer/gotags
 go get -v -u github.com/client9/misspell/cmd/misspell
+go get -v -u github.com/redefiance/go-find-references
+go get -v -u github.com/adjust/go-wrk
+go get -v -u github.com/uber/go-torch
 
 echo "Upgrading vim-go binaries ..."
 vim +GoUpdateBinaries +qall
