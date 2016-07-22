35c4012e499bc1927d4ae935678b481b02c4b63c
Eric Duncan <eduncan911+github@gmail.com>
2016-07-22 09:13:50 -0400

    re-ordering desktop scripts making it a bit more efficient as well

--- bin/desktop-stats.sh
+++ bin/desktop-stats.sh
@@ -1,38 +1,20 @@
 #/bin/bash
 
-# display todo-txt list
-if [[ -s /usr/local/bin/todo.sh ]]; then
-	TODO_HOME=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config | egrep -v "\+livevote|\-\-|tasks shown"`
-	TODO_HOME_COUNT="`echo "$TODO_HOME" | wc -l`"
-	echo "$TODO_HOME"
-	echo "--"
-	echo "TODO: $TODO_HOME_COUNT tasks shown"
-	echo 
-	echo 
-	TODO_LV=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config | grep "+livevote"`
-	TODO_LV_COUNT="`echo "$TODO_LV" | wc -l`"
-	echo "$TODO_LV"
-	echo "--"
-	echo "TODO: $TODO_LV_COUNT tasks shown"
+# istats
+if [[ -s /usr/local/bin/istats ]]; then
+	/usr/local/bin/istats extra
+	/usr/local/bin/istats fan speed
 	echo
 	echo
 fi
 
-# istats
-/usr/local/bin/istats extra
-/usr/local/bin/istats fan speed
-echo 
-echo 
-
-# top's output 
+# top's output
 #top -o cpu -l 1 -i 1 -n 20 | head -n 10
 top -l 1 -n 0 | head -n 10
-echo 
-echo 
+echo
+echo
 
 # file system stats
 df -h | egrep "disk|Filesystem"
-echo 
-echo 
-
-
+echo
+echo
---
+++ bin/desktop-todo.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+# display todo-txt list
+if [[ -s /usr/local/bin/todo.sh ]]; then
+	TODO=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config`
+	TODO_JW="`echo $TODO | grep '+jw'`"
+	TODO_JW_COUNT="`echo $TODO_JW | wc -l`"
+	echo "$TODO_JW"
+	echo "--"
+	echo "TODO: $TODO_JW_COUNT tasks shown"
+	echo
+	echo
+	TODO_HOME=`echo $TODO | egrep -v "\+jw|\-\-|tasks shown"`
+	TODO_HOME_COUNT="`echo $TODO_HOME | wc -l`"
+	echo "$TODO_HOME"
+	echo "--"
+	echo "TODO: $TODO_HOME_COUNT tasks shown"
+	echo
+	echo
+fi
