68500868d67ad7c2d1b0fc25f81c39c7d498cd75
Eric Duncan <eduncan911+github@gmail.com>
2016-07-24 11:39:12 -0400

    fixing up todo

--- bin/desktop-todo.sh
+++ bin/desktop-todo.sh
@@ -3,18 +3,20 @@
 # display todo-txt list
 if [[ -s /usr/local/bin/todo.sh ]]; then
 	TODO=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config`
-	TODO_JW="`echo $TODO | grep '+jw'`"
-	TODO_JW_COUNT="`echo $TODO_JW | wc -l`"
-	echo "$TODO_JW"
-	echo "--"
-	echo "TODO: $TODO_JW_COUNT tasks shown"
-	echo
-	echo
-	TODO_HOME=`echo $TODO | egrep -v "\+jw|\-\-|tasks shown"`
-	TODO_HOME_COUNT="`echo $TODO_HOME | wc -l`"
+    
+    TODO_HOME=`echo "$TODO" | egrep -v '\+jw|\+livevote|\-\-|tasks shown'`
+    TODO_HOME_COUNT="`echo "$TODO_HOME" | wc -l`"
 	echo "$TODO_HOME"
 	echo "--"
 	echo "TODO: $TODO_HOME_COUNT tasks shown"
+	echo
+	echo
+
+    TODO_JW=`echo "$TODO" | egrep '\+jw'`
+	TODO_JW_COUNT="`echo "$TODO_JW" | wc -l`"
+	echo "$TODO_JW"
+	echo "--"
+	echo "TODO: $TODO_JW_COUNT tasks shown"
 	echo
 	echo
 fi
