7dd293f3b3ccfae98517e5938f92c5e4f99ce9d0
Eric Duncan <eduncan911+github@gmail.com>
2016-08-10 10:41:43 -0400

    switching up desktop todos

--- bin/desktop-todo.sh
+++ bin/desktop-todo.sh
@@ -2,21 +2,21 @@
 
 # display todo-txt list
 if [[ -s /usr/local/bin/todo.sh ]]; then
-	TODO=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config`
+    TODO=`/usr/local/bin/todo.sh -d /Users/eric/.todo/config`
     
+    TODO_JW=`echo "$TODO" | egrep '\+jw'`
+    TODO_JW_COUNT="`echo "$TODO_JW" | wc -l`"
+    echo "$TODO_JW"
+    echo "--"
+    echo "TODO: $TODO_JW_COUNT tasks shown"
+    echo
+    echo
+
     TODO_HOME=`echo "$TODO" | egrep -v '\+jw|\+livevote|\-\-|tasks shown'`
     TODO_HOME_COUNT="`echo "$TODO_HOME" | wc -l`"
-	echo "$TODO_HOME"
-	echo "--"
-	echo "TODO: $TODO_HOME_COUNT tasks shown"
-	echo
-	echo
-
-    TODO_JW=`echo "$TODO" | egrep '\+jw'`
-	TODO_JW_COUNT="`echo "$TODO_JW" | wc -l`"
-	echo "$TODO_JW"
-	echo "--"
-	echo "TODO: $TODO_JW_COUNT tasks shown"
-	echo
-	echo
+    echo "$TODO_HOME"
+    echo "--"
+    echo "TODO: $TODO_HOME_COUNT tasks shown"
+    echo
+    echo
 fi
