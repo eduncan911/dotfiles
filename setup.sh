ba7e75599f37fdeede317ede86c5780329799f0a
Eric Duncan <me@eduncan911.com>
2017-06-16 10:44:07 -0700

    Force symlinks in setup.sh to allow repeated runs

--- setup.sh
+++ setup.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -o errexit
 
+# Install Chrome
+# Sign into Github, add SSH key for this machine.
+# download colors: git clone https://github.com/mbadolato/iTerm2-Color-Schemes
+
+# run setup script 
+
 # todo
 echo "Create aliases? [Y/n]"
 
 cd ~/
-ln -s .dotfiles/.Brewfile
-ln -s .dotfiles/.bashrc
-ln -s .dotfiles/.bash_aliases
-ln -s .dotfiles/.bash_profile
-ln -s .dotfiles/.gitconfig
-ln -s .dotfiles/.gitignore
-ln -s .dotfiles/.go-debug.json
-ln -s .dotfiles/.gometalinter.conf
-ln -s .dotfiles/.profile
-ln -s .dotfiles/.tmux
-ln -s .dotfiles/.tmux.conf
-ln -s .dotfiles/.vim
-ln -s .dotfiles/.vimrc
-ln -s .dotfiles/bin
+ln -sf .dotfiles/.Brewfile
+ln -sf .dotfiles/.bashrc
+ln -sf .dotfiles/.bash_aliases
+ln -sf .dotfiles/.bash_profile
+ln -sf .dotfiles/.gitconfig
+ln -sf .dotfiles/.gitignore
+ln -sf .dotfiles/.go-debug.json
+ln -sf .dotfiles/.gometalinter.conf
+ln -sf .dotfiles/.profile
+ln -sf .dotfiles/.tmux
+ln -sf .dotfiles/.tmux.conf
+ln -sf .dotfiles/.vim
+ln -sf .dotfiles/.vimrc
+ln -sf .dotfiles/bin
 
 echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
