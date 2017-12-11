ea0442c47360bcbc0c38637f94ac88dd30d7150f
Eric Duncan <me@eduncan911.com>
2017-12-10 21:15:08 -0500

    Increase git speeds by 400%
    This combined 4 git commands into 1 to increase
    the speed of git drastically.
    
    Now using 'git status --porcelain --branch --show-stash'
    and parsing it over and over again to increase speed.
    
    NOTE: There is a bug in git that --porcelain does not
    return the Stash count with --show-stash.  I have filed
    a bug with the Git team.  In the meantime, I have hardcoded
    the git stash list, which is very very slow.
    
    A future version will hopefully fix git status --porcelain
    to include Stash count, and therefore we can remove the
    extra "git stash list" command.
    
    But until then, you may want to disable the stash parts
    if it gets too slow.

--- bin/bash-powerline.sh
+++ bin/bash-powerline.sh
@@ -8,8 +8,9 @@ __powerline() {
     PS_SYMBOL_DARWIN=''
     PS_SYMBOL_LINUX='$'
     PS_SYMBOL_OTHER='%'
-    GIT_BRANCH_SYMBOL='⑂ '
+    GIT_BRANCH_SYMBOL='⑂'
     GIT_BRANCH_CHANGED_SYMBOL='+'
+    GIT_STASHED_SYMBOL="✹"
     GIT_NEED_PUSH_SYMBOL='⇡'
     GIT_NEED_PULL_SYMBOL='⇣'
 
@@ -56,16 +57,16 @@ __powerline() {
     BOLD="\[$(tput bold)\]"
 
     # what OS?
-    case "$(uname)" in
-        Darwin)
-            PS_SYMBOL=$PS_SYMBOL_DARWIN
-            ;;
-        Linux)
-            PS_SYMBOL=$PS_SYMBOL_LINUX
-            ;;
-        *)
-            PS_SYMBOL=$PS_SYMBOL_OTHER
-    esac
+    #case "$(uname)" in
+    #    Darwin)
+    #        PS_SYMBOL=$PS_SYMBOL_DARWIN
+    #        ;;
+    #    Linux)
+    #        PS_SYMBOL=$PS_SYMBOL_LINUX
+    #        ;;
+    #    *)
+    #        PS_SYMBOL=$PS_SYMBOL_OTHER
+    #esac
 
     __pwd_info() {
         printf $( pwd | \
@@ -120,17 +121,50 @@ __powerline() {
 
         local marks
         local bgcolor="$BG_CYAN"
-
+        local status="$($git_eng status --porcelain --branch)"
+        
         # branch is modified?
-        if [ -n "$($git_eng status --porcelain)" ]; then
-            marks+=" $GIT_BRANCH_CHANGED_SYMBOL"
+        local modified=$(echo "$status" | wc -l)
+        if [ "$modified" -ne 1 ]; then
+        #    marks+=" $GIT_BRANCH_CHANGED_SYMBOL"
             bgcolor="$BG_YELLOW"
         fi
 
+        # do we have anything unstaged or staged
+        local staging
+        local uadd=$(grep -c '^??' <<< "$status")
+        local umod=$(grep -c '^ M' <<< "$status")
+        local udel=$(grep -c '^ D' <<< "$status")
+        local sadd=$(grep -c '^A ' <<< "$status")
+        local smod=$(grep -c '^M ' <<< "$status")
+        local sdel=$(grep -c '^D ' <<< "$status")
+        if [ "$uadd" -ne 0 ] || [ "$umod" -ne 0 ] || [ "$udel" -ne 0 ]; then
+            staging=" (+$uadd ~$umod -$udel"
+        fi
+        if [ "$sadd" != 0 ] || [ "$smod" != 0 ] || [ "$sdel" != 0 ]; then
+            if [ -n "$staging" ]; then
+                staging+="|+$sadd ~$smod -$sdel)"
+            else
+                staging=" (+0 ~0 -0|+$sadd ~$smod -$sdel)"
+            fi
+        else
+            if [ -n "$staging" ]; then
+                staging+=")"
+            fi
+        fi
+ 
+        # do we have any stashes?
+        local stashed
+        local stash="$($git_eng stash list | wc -l)"
+        if [ "$stash" != 0 ]; then
+            #marks+=" $GIT_STASHED_SYMBOL"
+            stashed=" [$stash]"
+        fi
+
         # how many commits local branch is ahead/behind of remote?
-        local stat="$($git_eng status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
-        local aheadN="$(echo $stat | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-        local behindN="$(echo $stat | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
+        local bstatus=$(echo "$status" | grep '^##' | grep -o '\[.\+\]$')
+        local aheadN=$(echo "$bstatus" | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')
+        local behindN=$(echo "$bstatus" | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')
 
         # what bg color
         if [ -n "$aheadN" ]; then
@@ -146,7 +180,7 @@ __powerline() {
         fi
 
         # print the git branch segment without a trailing newline
-        printf "$bgcolor $GIT_BRANCH_SYMBOL$branch$marks "
+        printf "$bgcolor $branch$staging$stashed$marks "
     }
 
     ps1() {
