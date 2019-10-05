d1b7a76fc488ac6198e6e8a26779edd84cf7bc33
Eric Duncan <me@eduncan911.com>
2019-10-05 09:13:56 -0400

    Add Custom Pinentry Logic for Passphrases

--- bin/pinentry.sh
+++ bin/pinentry.sh
@@ -5,7 +5,7 @@ set -eu
 # if we are in X11 or tty to determine which
 # to show.
 
-PINENTRY_TERMINAL='/usr/bin/pinentry-curses'
+PINENTRY_TERMINAL='/usr/bin/pinentry-tty'
 PINENTRY_X11='/usr/bin/pinentry-x11'
 
 if [ -n "${DISPLAY-}" -a -z "${TERM-}" ]; then
