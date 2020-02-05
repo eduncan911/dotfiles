1c3954c4896597e28c059c83544ff4c96ac540a5
Eric Duncan <me@eduncan911.com>
2020-02-05 03:00:40 -0500

    Bring back to life upgrade-go.sh

--- bin/upgrade-go.sh
+++ bin/upgrade-go.sh
@@ -10,15 +10,7 @@ set -o errexit
 #
 # but, it will skip this process if you do not have this path.
 function _upgrade_go_binary {
-    local GO_LATEST_VERSION=`curl -silent https://storage.googleapis.com/golang/ | \
-        grep -Eo '<Key>[^<]*</Key>' | \
-        cut -d'>' -f2 | \
-        cut -d'<' -f1 | \
-        grep '.src.tar.gz$' | \
-        grep -Ev '(rc|beta)[0-9]' | \
-        sed 's/\.src\.tar\.gz//' | \
-        sort -n -r | \
-        head -n 1`
+    local GO_LATEST_VERSION=`curl --silent https://golang.org/VERSION?m=text`
     if [[ "$GO_LATEST_VERSION" == "" ]]; then
         echo "Skipping Go binary upgrade (no version found)."
         return
@@ -35,7 +27,7 @@ function _upgrade_go_binary {
     local GO_LATEST_FILENAME="$GO_LATEST_VERSION.$(uname -s | \
         tr '[:upper:]' '[:lower:]')-amd64.tar.gz"
     echo "Downloading $GO_LATEST_FILENAME to ~/.go/$GO_LATEST_FILENAME"
-    wget https://storage.googleapis.com/golang/$GO_LATEST_FILENAME -O ~/.go/$GO_LATEST_FILENAME
+    wget -q https://dl.google.com/go/$GO_LATEST_FILENAME -O ~/.go/$GO_LATEST_FILENAME
 
     echo "Installing $GO_LATEST_FILENAME" 
     cd ~/.go
@@ -47,8 +39,7 @@ function _upgrade_go_binary {
     go version
 }
 if [[ -d ~/.go ]]; then 
-    #_upgrade_go_binary
-    echo "Skipped upgrading binary. source is now paged."
+    _upgrade_go_binary
 else
     echo "No custom ~/.go/ detected.  Using system installed version."
 fi
@@ -60,24 +51,34 @@ if [[ ! -x "$(which go)" ]]; then
 fi
 
 echo "Upgrading go tools used by IDEs and vim..."
+printf "# golang.org/x/tools/...\n"
 go get -u golang.org/x/tools/...
+printf "# golang.org/x/tools/cmd/...\n"
 go get -u golang.org/x/tools/cmd/...
-go get -u github.com/golang/lint/golint
-#go get -u github.com/redefiance/go-find-references
+# go get -u golang.org/x/lint/golint
+# go get -u github.com/golang/lint/golint
+# go get -u github.com/redefiance/go-find-references
+printf "# github.com/jstemmer/gotags\n"
 go get -u github.com/jstemmer/gotags
+printf "# github.com/client9/misspell/cmd/misspell\n"
 go get -u github.com/client9/misspell/cmd/misspell
+printf "# github.com/adjust/go-wrk\n"
 go get -u github.com/adjust/go-wrk
-go get -u github.com/uber/go-torch
+printf "# github.com/rogpeppe/godef\n"
 go get -u github.com/rogpeppe/godef
+printf "# github.com/cweill/gotests/...\n"
 go get -u github.com/cweill/gotests/...
+printf "# github.com/lukehoban/go-outline\n"
 go get -u github.com/lukehoban/go-outline
+printf "# github.com/nsf/gocode && gocode close\n"
 go get -u github.com/nsf/gocode && gocode close
+printf "# github.com/ramya-rao-a/go-outline\n"
 go get -u github.com/ramya-rao-a/go-outline
+printf "# github.com/sourcegraph/go-langserver\n"
 go get -u github.com/sourcegraph/go-langserver
 
-echo "Upgrading gometalinter linter tools used by IDEs and vim ..."
-go get -u github.com/alecthomas/gometalinter
-gometalinter --install --update
+echo "Upgrading golangci-lint tools used by IDEs and vim ..."
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.3
 
 echo "Upgrading vim-go binaries ..."
 vim +GoUpdateBinaries +qall
@@ -85,8 +86,6 @@ vim +GoUpdateBinaries +qall
 echo "Upgrade personal utilities built with Go ..."
 echo "  godoc2ghmd          # converts go docs to GitHub README.md markdown"
 go get -u github.com/eduncan911/godoc2ghmd
-echo "  godoc2md            # converts go docs to README.md markdown"
-go get -u github.com/davecheney/godoc2md
 echo "  gostatus            # checks status of packages in GOPATH"
 go get -u github.com/shurcooL/gostatus
 echo "  gofresh             # checks status of packages in GOPATH"
